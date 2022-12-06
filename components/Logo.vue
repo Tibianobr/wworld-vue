457b0b50f496fe967c0799479931a66b0b74021a
bserrato <brennoserrato@alunos.utfpr.edu.br>
2022-12-06 21:30:28 +0000

    :sparkles: :art: Logo component

---
+++ components/Logo.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="logo-header">
+        <span class="material-icons-outlined">
+        wb_cloudy
+        </span>
+    WWeather
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Logo"
+}
+</script>
+
+<style lang="scss">
+.logo-header {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  color: $text-primary;
+  font-size: 32px;
+  font-weight: 900;
+
+  span {
+    font-size: 32px !important;
+  }
+}
+</style>
